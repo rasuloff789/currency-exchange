19b1364bde0c28747b6c54be30a83af545ee98e3
rasuloff789 <rasuloff789@gmail.com>
2020-10-29 15:10:08 +0500

    project end

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,47 @@
-alert(`Valyuta counter Qo'rqma olib ketishmaydi :))`)
 var exchangeForm = document.querySelector('.exchange-form');
 var result = exchangeForm.querySelector('.value-of-exchange');
 var submitBtn = exchangeForm.querySelector('.submit-btn');
-var numberExchange =exchangeForm.querySelector('.number-of-exchange');
-var textIfMinus = exchangeForm.querySelector('.text-minus');
-// var textverify = exchangeForm.querySelector('.textVerify');
+var numberExchange = exchangeForm.querySelector('.number-of-exchange');
+var textMinus = document.querySelector('.text-minus');
+var textNot = document.querySelector('.text-not-number');
+
 submitBtn.addEventListener('click' , function(evt){
   evt.preventDefault();
-  var numberResult = parseInt(numberExchange.value , 10) ;
-  var currencyType = document.querySelector('.currency-type');
-  var currencyIndex = currencyType.selectedIndex;
-  var currencySelect = currencyType.querySelectorAll('OPTION')[currencyIndex].value ;
-  var currencyValue = parseInt(currencySelect , 10) ;
-  if ( numberResult > 0 ){
-    result.textContent = Math.round(numberResult * currencyValue) ;
-    textIfMinus.classList.add('d-none'); 
-    textIfMinus.classList.remove('d-block'); 
-    
-  }else{
-    textIfMinus.classList.remove('opacity-0'); 
-    textIfMinus.classList.add('d-block'); 
+  var currencyValue = parseFloat(document.querySelector('.currency-type').value.trim() , 10);
+  var exchangeSumm = parseFloat(numberExchange.value.trim() , 10);
+  if(exchangeSumm.textContent = ""){
+    textNot.classList.add('d-block');
+    textMinus.classList.remove('d-block');
+    numberExchange.classList.add('is-invalid');
+    result.textContent = "0";
+    return;
   }
+  
+  if ( currencyValue === 0 ){
+    alert('Valyutani tanlang');
+    return;
+  }
+  
+  if(isNaN(exchangeSumm)){
+    textNot.classList.add('d-block');
+    textMinus.classList.remove('d-block');
+    numberExchange.classList.add('is-invalid');
+    result.textContent = "0";
+    return;
+  }
+  
+  if(exchangeSumm < 0){
+    textNot.classList.remove('d-block');
+    textMinus.classList.add('d-block');
+    numberExchange.classList.add('is-invalid');
+    result.textContent = "0";
+    return;
+  }
+  
+  resultNumber = Math.round(currencyValue * exchangeSumm) ;
+  result.textContent = resultNumber;
+  textNot.classList.remove('d-block');
+  textMinus.classList.remove('d-block');
+  numberExchange.classList.remove('is-invalid');
+  
 })
